d7d1e7dc334e347a9b277c54fe284657eca80b4d
Mauricio Picca <mpicca83@hotmail.com>
2023-07-19 20:01:59 -0300

    Agregado de Funciones de Orden Superior

--- main.js
+++ main.js
@@ -1,176 +1,132 @@
 let cliente = ''
 let codigoProducto = 0
 let producto = {}
-let productosComprados = []
-let detalleProductos = ''
+let carrito = []
+let totalCarrito = 0
+let detalleProductos = []
 let cantidad = 0
-let subTotal = 0
-let seguirComprando = true
+let seguirComprando = false
 let formaPago = ''
 let formaEnvio = ''
-let costoEnvio = 0
+let costoEnvio = 500
 const descuento = 0.1
 let importeDescuento = 0
 let direccion = ''
+let existeProducto = false
+
+//Capturar Boton
+const btnRealizarCompra = document.querySelector('#realizarCompra')
+btnRealizarCompra.onclick = () => {
+    inicio()
+}
 
 const inicio = () =>{
-    cliente = prompt('Bienvenido a la tienda on-line del Rey de la Limpieza, por favor ingrese su nombre:')
-    subTotal = 0
-    if(cliente===null || cliente===''){
-       alert('El nombre ingresado no es valido.')
-       inicio()
-    }else{
-        productosComprados = []
-        detalleProductos = ''
-        comprar()
-        envio()
-        pagar()
-        alert(`Muchas gracias por su compra! 
-        Se adjunta un detalle de la misma:
-        - Cliente: ${cliente}
-        - Productos: ${detalleProductos}
-        - Forma de envío: ${formaEnvio}
-        - Dirección de envío: ${direccion}
-        - Costo por envío: $${costoEnvio}
-        - Forma de pago: ${formaPago}
-        - Descuento por pago de contado: -$${importeDescuento}
-        - Total a Pagar: $${subTotal}`)
-    }
-}
-
-const ingresarProducto = () =>{
-    codigoProducto = parseInt(prompt('Ingrese el código del producto que desea agregar al carrito de compra.'))
-    if(codigoProducto === null || codigoProducto<=0 || isNaN(codigoProducto)){
-        alert('El código ingresado no es valido.')
-        ingresarProducto()
-    }
-    return codigoProducto
-}
-
-const buscarProducto = () =>{
-    switch (codigoProducto) {
-        case 1:
-            producto.nombre = 'Jabón líquido para ropa x 1L'
-            producto.precio = 1000
-            break;
-        case 2:
-            producto.nombre = 'Suavizante para ropa x 1L'
-            producto.precio = 900
-            break;
-        case 3:
-            producto.nombre = 'Quitamanchas para ropa x uni'
-            producto.precio = 500
-            break;
-        case 4:
-            producto.nombre = 'Detergente x 1L'
-            producto.precio = 450
-            break;
-        case 5:
-            producto.nombre = 'Desinfectante para pisos x 1L'
-            producto.precio = 700
-            break;
-        case 6:
-            producto.nombre = 'Lavandina x 1L'
-            producto.precio = 400
-            break;
-        case 7:
-            producto.nombre = 'Desengrasante x 1L'
-            producto.precio = 750
-            break;
-        case 8:
-            producto.nombre = 'Esponja de cocina x uni'
-            producto.precio = 150
-            break;
-        case 9:
-            producto.nombre = 'Mopa para pisos x uni'
-            producto.precio = 400
-            break;
-        case 10:
-            producto.nombre = 'Trapo de pisos x uni'
-            producto.precio = 300
-            break;
-        case 11:
-            producto.nombre = 'Rejilla x uni'
-            producto.precio = 200
-            break;
-        case 12:
-            producto.nombre = 'Limpia vidrios x 1L'
-            producto.precio = 450
-            break;
-        case 13:
-            producto.nombre = 'Desinfectante para baños x 1L'
-            producto.precio = 600
-            break;
-        case 14:
-            producto.nombre = 'Escoba x uni'
-            producto.precio = 500
-            break;
-        case 15:
-            producto.nombre = 'Secador de pisos'
-            producto.precio = 500
-            break;
-        default:
-            alert('El código ingresado no corresponde a un producto válido, por favor repita nuevamente la operación.')
-            producto.nombre = ''
-            producto.precio = 0
-            ingresarProducto()
-            break;
-    }
-    return producto
-}
-
-const ingresarCantidad = () =>{
-    cantidad = parseInt(prompt('Ingrese la cantidad de ' + producto.nombre + ' que desea agregar al carrito de compra'))
-    if(cantidad === null || cantidad<=0 || isNaN(cantidad)){
-        alert('El código ingresado no es valido.')
-        ingresarCantidad()
-    }
-    return cantidad
+    do{
+        cliente = prompt('Bienvenido a la tienda on-line del Rey de la Limpieza.\nPor favor ingrese su nombre:')
+        if(cliente===null || cliente===''){
+            alert('El nombre ingresado no es valido.')
+        }
+    }while(cliente===null || cliente==='')
+    comprar()
 }
 
 const comprar = () =>{
     do{
-        ingresarProducto()
-        buscarProducto()
-        ingresarCantidad()
+        do{
+            codigoProducto = parseInt(prompt('Ingrese el código del producto que desea agregar al carrito de compra.'))
+            existeProducto = productos.some(productos => productos.id == codigoProducto)
+            if(!existeProducto){
+                alert('El código ingresado no es valido.')
+            }
+        }while(!existeProducto)
 
-        subTotal += producto.precio * cantidad
-
-        detalleProductos += `\n            * ${cantidad} x ${producto.nombre} = $${producto.precio * cantidad}`
-
-        seguirComprando = confirm('El importe total del carrito de compra es de $'+ subTotal +'. Desea agregar otro producto?')
+        producto = productos.find(producto => producto.id == codigoProducto)
+    
+        do{
+            cantidad = parseInt(prompt('Ingrese la cantidad de *' + producto.nombre + '* que desea agregar al carrito de compra'))        
+            if(cantidad === null || cantidad<=0 || isNaN(cantidad)){
+                alert('La cantidad ingresada no es valida.')
+            }else{
+                agregarCarrito(producto, producto.id, cantidad)
+            }
+        }while(cantidad === null || cantidad<=0 || isNaN(cantidad))
+        
+        totalCarrito = carrito.reduce((acc, producto) => acc + (producto.cantidad * producto.precio), 0)
+        seguirComprando = confirm(`El importe acumulado en el carrito es de $${totalCarrito}\n¿Desea agregar otro producto?`)
 
     }while(seguirComprando)
+    confirmacion()
+}
+
+const agregarCarrito = (producto, id, cantidad) =>{
+    const productoRepetido = carrito.find(productoRe => productoRe.id == id)
+    if (!productoRepetido) {
+        producto.cantidad = cantidad
+        carrito.push(producto)
+    } else {
+        productoRepetido.cantidad += cantidad
+    }
+}
+
+const confirmacion =() =>{
+    carrito.forEach((producto) =>{
+        detalleProductos += `\n - Producto: ${producto.nombre} | Cantidad: ${producto.cantidad} = $${producto.precio * producto.cantidad}`
+    })
+    const confirmar = confirm(`Detalle del carrito: 
+        ${detalleProductos}
+        \nImporte total del carrito: $${totalCarrito}
+        \nPresione "Aceptar" para confirmar o "Cancelar" para anular la compra.`
+    )
+    if(confirmar){
+        subTotal = totalCarrito
+        envio()
+    }else{
+        alert("La compra fue cancelada.\nQue tenga un buen día.")
+        carrito = []
+    }
 }
 
 const envio = () =>{
-    formaEnvio = prompt('El envío a domicilio tiene un costo de $500. Ingrese "SI" para enviar a domicilio o ingrese "NO" para retirar el pedido.').toUpperCase()
-    if(formaEnvio==="SI"){
-        direccion = prompt('Ingrese la dirección para que le enviemos el pedido.')
+    formaEnvio = confirm('El envío a domicilio tiene un costo de $500.\nIngrese "Aceptar" para enviar a domicilio o ingrese "Cancelar" para retirar el pedido.')
+    if(formaEnvio){
+        do{
+            direccion = prompt('Usted eligió envió a domicilio.\nIngrese la dirección para que le enviemos el pedido.')
+        }while(direccion === null || direccion === '')
         costoEnvio = 500
         subTotal += costoEnvio
         formaEnvio = 'ENVÍO A DOMICILIO'
-    }else if(formaEnvio==="NO"){
+    }else{
         alert('Usted eligió retiro por local.')
-        formaEnvio = 'RETIRO POR EL LOCAL.'
+        formaEnvio = 'RETIRO POR EL LOCAL'
         direccion = ''
         costoEnvio = 0
-    }else{
-        alert('El valor ingresado no es correcto.')
-        envio()
     }
+    pagar()
 }
 
 const pagar = () =>{
-    formaPago = prompt('Si realiza el pago de contado se realizará un descuento del 10% sobre el total de la compra. Elija la forma de pago. ¿Efectivo o Tarjeta?').toUpperCase()
-    if (formaPago==="EFECTIVO"){
+    formaPago = confirm('Si realiza el pago de contado tendrá un descuento del 10% sobre el total de la compra. \nElija la forma de pago:\n"Aceptar" para pago de Contado\n"Cancelar" para pago con Tarjeta')
+    if (formaPago){
         importeDescuento = subTotal*descuento
         subTotal = subTotal - importeDescuento
-        alert('Se aplico un descuento del 10%. El total a pagar es $'+subTotal)
-    }else if(formaPago==="TARJETA"){
-        alert('El pago se realizará con tarjeta. El total a pagar es $'+subTotal)
+        formaPago = "Efectivo"
+        alert('Se aplico un descuento del 10%.\nEl total a pagar es $'+subTotal)
     }else{
-        alert('El valor ingresado no es correcto.')
-        pagar()
+        formaPago = "Tarjeta"
+        alert('El pago se realizará con tarjeta.\nEl total a pagar es $'+subTotal)
     }
+    mostrarDetalleCompra()
+}
+
+const mostrarDetalleCompra = () => {
+    alert(`Muchas gracias por su compra! 
+    Se adjunta un detalle de la misma:\n
+Cliente: ${cliente}
+${detalleProductos}\n
+Forma de envío: ${formaEnvio}
+Dirección de envío: ${direccion}
+Costo por envío: $${costoEnvio}
+Forma de pago: ${formaPago}
+Descuento: -$${importeDescuento}
+Total a Pagar: $${subTotal}`)
 }
---
+++ stock.js
@@ -0,0 +1,93 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: 'Jabón líquido para ropa x 1L',
+        precio: 1000,
+        cantidad:0
+    },
+    {
+        id: 2,
+        nombre: 'Suavizante para ropa x 1L',
+        precio: 900,
+        cantidad:0
+    },
+    {
+        id: 3,
+        nombre: 'Quitamanchas para ropa x uni',
+        precio: 500,
+        cantidad:0
+    },
+    {
+        id: 4,
+        nombre: 'Detergente x 1L',
+        precio: 450,
+        cantidad:0
+    },
+    {
+        id: 5,
+        nombre: 'Desinfectante para pisos x 1L',
+        precio: 700,
+        cantidad:0
+    },
+    {
+        id: 6,
+        nombre: 'Lavandina x 1L',
+        precio: 400,
+        cantidad:0
+    },
+    {
+        id: 7,
+        nombre: 'Desengrasante x 1L',
+        precio: 750,
+        cantidad:0
+    },
+    {
+        id: 8,
+        nombre: 'Esponja de cocina x uni',
+        precio: 150,
+        cantidad:0
+    },
+    {
+        id: 9,
+        nombre: 'Mopa para pisos x uni',
+        precio: 400,
+        cantidad:0
+    },
+    {
+        id: 10,
+        nombre: 'Trapo de pisos x uni',
+        precio: 300,
+        cantidad:0
+    },
+    {
+        id: 11,
+        nombre: 'Rejilla x uni',
+        precio: 200,
+        cantidad:0
+    },
+    {
+        id: 12,
+        nombre: 'Limpia vidrios x 1L',
+        precio: 450,
+        cantidad:0
+    },
+    {
+        id: 13,
+        nombre: 'Desinfectante para baños x 1L',
+        precio: 600,
+        cantidad:0
+    },
+    {
+        id: 14,
+        nombre: 'Escoba x uni',
+        precio: 500,
+        cantidad:0
+    },
+    {
+        id: 15,
+        nombre: 'Secador de pisos',
+        precio: 500,
+        cantidad:0
+    },
+]
